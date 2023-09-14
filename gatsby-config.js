f19c45d680c3c5aaef0148caacdeb822a333c0a3
jackson <jackson.onyango@undp.org>
2023-09-14 10:43:38 +0300

    Add top nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://sdgailab.org/',
-    title: 'Home - SDG AI Lab',
+    title: 'SDG AI Lab',
     description: 'SDG AI Lab Website',
     image: '',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    'gatsby-plugin-image',
+    // 'gatsby-plugin-sharp',
+    // 'gatsby-transformer-sharp',
+  ],
 };
