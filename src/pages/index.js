cdf5ccd57dcff782db189b8e12d0fe88df5fdcfe
jackson <jackson.onyango@undp.org>
2023-09-15 13:19:23 +0300

    Volunteer section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
 import { Layout } from '../components/layout';
-import { hero, container } from '../assets/css/index.module.css';
+import { hero, container, crosshair } from '../assets/css/index.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  faCoffee,
-  faArrowRight,
   faCaretRight,
+  faCrosshairs,
+  faLocationArrow,
+  faShareNodes,
+  faUserGroup,
 } from '@fortawesome/free-solid-svg-icons';
 
 const IndexPage = () => {
@@ -35,7 +37,7 @@ const IndexPage = () => {
       </div>
       <section
         className="clean-block clean-info dark"
-        style={{ padding: '30px' }}
+        style={{ padding: '80px' }}
       >
         <div className="container">
           <div className="block-heading">
@@ -67,6 +69,94 @@ const IndexPage = () => {
               reports, presentations
               <br />
             </p>
+          </div>
+        </div>
+      </section>
+      <section
+        className="clean-block clean-info dark"
+        style={{ padding: '80px', backgroundColor: '#fff' }}
+      >
+        <div className="container">
+          <div className="block-heading">
+            <h2 style={{ textAlign: 'center', color: '#3b99e0' }}>
+              Our Volunteer Data Scientist Initiative
+            </h2>
+            <p>
+              <strong>·&nbsp;</strong>SDG AI Lab mobilizes volunteer data
+              scientists via UNV-IICPSD Digital Transformation Partnership.
+              <br />
+              <br />
+              <strong>·&nbsp;</strong>The Lab engages online volunteers to SDG
+              relevant research streams, manages their work and facilitates
+              their relationship with UNDP units.
+            </p>
+          </div>
+          <div className="row justify-content-center">
+            <div
+              className="col-md-5 feature-box"
+              style={{ display: 'flex', gap: '20px' }}
+            >
+              <FontAwesomeIcon
+                icon={faCrosshairs}
+                size="2x"
+                style={{ color: '#3b99e0' }}
+              />
+              <p>
+                Connecting UNDP teams and data scientists to address development
+                challenges
+              </p>
+            </div>
+            <div
+              className="col-md-5 feature-box"
+              style={{ display: 'flex', gap: '20px' }}
+            >
+              <FontAwesomeIcon
+                icon={faShareNodes}
+                size="2x"
+                style={{ color: '#3b99e0' }}
+              />
+              <p>
+                Agile framework for small research &amp; development teams:
+                task-based, time-boxed iterations, asynchronous cooperation,
+                Scrum-based project management
+              </p>
+            </div>
+            <div
+              className="col-md-5 feature-box"
+              style={{ display: 'flex', gap: '20px' }}
+            >
+              <FontAwesomeIcon
+                icon={faLocationArrow}
+                size="2x"
+                style={{ color: '#3b99e0' }}
+              />
+              <p>
+                Environment for remote team collaboration: <br />- Microsoft
+                Teams Channel for Communication&nbsp;
+                <br />-
+                <a href="https://github.com/SDG-AI-Lab">
+                  Github Group Repository
+                </a>
+                for Code Review
+              </p>
+            </div>
+            <div
+              className="col-md-5 feature-box"
+              style={{ display: 'flex', gap: '20px' }}
+            >
+              <FontAwesomeIcon
+                icon={faUserGroup}
+                size="2x"
+                style={{ color: '#3b99e0' }}
+              />
+              <p>
+                - 1 Technical Advisor
+                <br />- 3 Full-Time Data Scientists
+                <br />- Volunteer Data Scientists
+                <br />- 1 Full-Time Partnership &amp; Outreach Analyst
+                <br />
+              </p>
+            </div>
           </div>
         </div>
       </section>
