9e9dbdb077093a1efcd16b7247535afe5c66006a
jackson <jackson.onyango@undp.org>
2023-09-15 11:54:17 +0300

    Fix alignments

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,31 +8,26 @@ const IndexPage = () => {
     <Layout>
       <div className={hero}>
         <div className={container}>
-          <div
-            className="clean-block clean-hero"
-            style={{ position: 'absolute', width: '100%', height: '100%' }}
-          >
-            <div className="text">
-              <img src="/static/51e94a73e7b664717bc78ab1a9cba548/Webp.net-resizeimage.png" />
-              <h2>
-                Harnessing the potential of Artificial Intelligence for
-                Sustainable Development
-              </h2>
-              <p>
-                A joint initiative of UNDP
-                <strong>Nature, Climate, and Energy Team</strong>, UNDP
-                <strong>Finance Sector Hub</strong>, and UNDP
-                <strong>
-                  Istanbul International Center for Private Sector in
-                  Development
-                </strong>
-                (IICPSD)
-              </p>
-            </div>
+          <div>
+            <img src="/static/51e94a73e7b664717bc78ab1a9cba548/Webp.net-resizeimage.png" />
           </div>
+
+          <h2>
+            Harnessing the potential of Artificial Intelligence for Sustainable
+            Development
+          </h2>
+          <p>
+            A joint initiative of UNDP
+            <strong>Nature, Climate, and Energy Team</strong>, UNDP
+            <strong>Finance Sector Hub</strong>, and UNDP
+            <strong>
+              Istanbul International Center for Private Sector in Development
+            </strong>
+            (IICPSD)
+          </p>
         </div>
-        <Link to="about"> About this site</Link>
       </div>
+      <Link to="about"> About this site</Link>
     </Layout>
   );
 };
