7fc6e503966e26b60c973c609603a2daffc1cc68
jackson <jackson.onyango@undp.org>
2023-09-15 14:39:57 +0300

    Add sponsors

--- src/pages/index.js
+++ src/pages/index.js
@@ -160,6 +160,74 @@ const IndexPage = () => {
           </div>
         </div>
       </section>
+      <section
+        className="clean-block clean-info dark"
+        style={{ padding: '80px' }}
+      >
+        <div className="container">
+          <div className="block-heading">
+            <h2 style={{ textAlign: 'center', color: '#3b99e0' }}>
+              Our Supporters and Partners
+            </h2>
+          </div>
+          <div className="clean-block add-on sponsors">
+            <a href="https://www.thegef.org/">
+              <img src="/static/ba8efbe428ae1f34ada07cbe331ef725/Picture1.png" />
+            </a>
+            <a href="https://www.theglobalfund.org/en/">
+              <img src="/static/e7eb5afbbf6a2cc6a284b34af052e20f/Picture2.png" />
+            </a>
+            <a href="https://www.undp.org/content/undp/en/home/2030-agenda-for-sustainable-development/planet.html">
+              <img src="/static/10cfba2b436fbec880be544ce9f69726/Picture3.png" />
+            </a>
+            <a href="https://www.greenclimate.fund/">
+              <img src="/static/7bde31100efa7c2d168a198ef769abb9/gcf-removebg-preview.png" />
+            </a>
+            <a href="https://www.connectingbusiness.org/home">
+              <img src="/static/0b6cbbb20efc9d76cc3e2d02e2d20462/Picture5.png" />
+            </a>
+            <a href="https://www.businesscalltoaction.org/">
+              <img src="/static/df78c943d89c661310ac82b84d1251d2/Picture6.png" />
+            </a>
+            <a href="https://www.unocha.org/">
+              <img src="/static/11994c67be73f9c98a1245da8024ef14/Picture7.png" />
+            </a>
+            <a href="https://www.ppmi.lt/">
+              <img src="/static/a9d7a6cb0ab779f17765ab7503568877/Picture8.png" />
+            </a>
+            <a href="https://www.unv.org/">
+              <img src="/static/ffae5359a9a6f22f667bf6140f050380/Picture10.png" />
+            </a>
+            <a href="https://www.iicpsd.undp.org/content/istanbul/en/home.html">
+              <img src="/static/01a167978fdbc24ae721094078e628dc/Picture11.png" />
+            </a>
+          </div>
+          <p style={{ textAlign: 'center' }}>
+            The lab is grateful to the governments of our fully-funded
+            volunteers for their support:
+          </p>
+          <div className="clean-block add-on sponsors">
+            <a href="https://www.fmprc.gov.cn/mfa_eng/">
+              <img src="/static/f328cb5770745cbccafc48b765ea4f44/Picture12.png" />
+            </a>
+            <a href="https://www.fmprc.gov.cn/mfa_eng/"></a>
+            <a href="https://www.gov.kz/memleket/entities/mfa?lang=en">
+              <img src="/static/7a831d48c4af1fad17744cb3cce19854/Picture13.png" />
+            </a>
+            <a href="http://www.mofa.go.kr/eng/index.do">
+              <img src="/static/a5002b9120c5921cb80d54e8e4e89822/Picture14.png" />
+            </a>
+            <a href="http://www.mfa.gov.tr/">
+              <img src="/static/40a744321e986b4c3863fbcbb7157bb4/Picture15.png" />
+            </a>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-md-6">
+              <div className="getting-started-info"></div>
+            </div>
+          </div>
+        </div>
+      </section>
       <Link to="about" style={{ display: 'block', marginTop: '500px' }}>
         {' '}
         About this site
